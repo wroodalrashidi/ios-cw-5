6352d9b24457e0b9b31c37e029c16510c13eadec
wroodalrashidi <66529914+wroodalrashidi@users.noreply.github.com>
2020-06-30 15:01:15 +0300

    h

--- X-0/X-0/ViewController.swift
+++ X-0/X-0/ViewController.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var b1: UIButton!
     @IBOutlet weak var b2: UIButton!
     @IBOutlet weak var b3: UIButton!
@@ -21,69 +22,144 @@ class ViewController: UIViewController {
     @IBOutlet weak var b9: UIButton!
     
     
+    
+    @IBOutlet weak var xScore: UILabel!
+    @IBOutlet weak var oScore: UILabel!
+     var xcounter = 0
+    var ocounter = 0
+    
     @IBOutlet weak var turnLabel: UILabel!
     
+    var backroundMusic: AVAudioPlayer?
+    var catSound: AVAudioPlayer?
+    var dogSound: AVAudioPlayer?
+    var whistleSound: AVAudioPlayer?
+    //var background: [UIColor] =
+    func playMusic() {
+        let path = Bundle.main.path(forResource: "tomAndJerry.mp3", ofType:nil)!
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            backroundMusic = try AVAudioPlayer(contentsOf: url)
+            backroundMusic?.play()
+            backroundMusic?.numberOfLoops = -1
+
+        } catch {
+            
+        }
+    }
+    
+    func playCat() {
+        let path = Bundle.main.path(forResource: "cat.mp3", ofType:nil)!
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            catSound = try AVAudioPlayer(contentsOf: url)
+            catSound?.play()
+        } catch {
+            
+        }
+    }
+    
+    func playDog() {
+        let path = Bundle.main.path(forResource: "dog.mp3", ofType:nil)!
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            dogSound = try AVAudioPlayer(contentsOf: url)
+            dogSound?.play()
+        } catch {
+            
+        }
+    }
+    
+    func playWhistle() {
+        let path = Bundle.main.path(forResource: "Whistling.mp3", ofType:nil)!
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            whistleSound = try AVAudioPlayer(contentsOf: url)
+            whistleSound?.play()
+        } catch {
+            
+        }
+    }
     
     var buttons: [UIButton] = []
     var counter = 0
     
     override func viewDidLoad() {
         buttons = [b1, b2, b3, b4, b5, b6, b7, b8, b9]
+        playMusic()
     }
-
+    
     @IBAction func press(_ sender: UIButton) {
         print("تم الضغط علي!")
         print(counter)
         
         if counter % 2 == 0{
-            sender.setTitle("X", for: .normal)
+            playCat()
+            AudioServicesPlaySystemSound(kSystemSoundID_Vibrate)
+            
+            sender.setTitle("😼", for: .normal)
             sender.setTitleColor(.white, for: .normal)
-            turnLabel.text = "O Turn"
+            turnLabel.text = "🐶 Turn"
         }
         else {
-             sender.setTitle("O", for: .normal)
-             sender.setTitleColor(.black, for: .normal)
-            turnLabel.text = "X Turn"
-
+            playDog()
+            AudioServicesPlaySystemSound(kSystemSoundID_Vibrate)
+            sender.setTitle("🐶", for: .normal)
+            sender.setTitleColor(.black, for: .normal)
+            turnLabel.text = "😼 Turn"
+            
         }
-       counter += 1
+        
+       
         sender.isEnabled = false
+        if counter == 8
+        {
+            self.alert(winner: "DRRRRRRRAW")
+        }
+         counter += 1
+        Winning(winner: "😼")
+        Winning(winner: "🐶")
         
-        Winning(winner: "X")
-        Winning(winner: "O")
-
     }
-        
-        
+   
+   
+    
+    
+    
     @IBAction func resetTapped() {
         self.restartGame()
+        ocounter = 0
+        xcounter = 0
+        oScore.text = ""
+        xScore.text = ""
     }
-
     
-        
+    
+    
     func Winning(winner: String)
+    {
+        if  (b1.titleLabel?.text == winner && b2.titleLabel?.text == winner && b3.titleLabel?.text == winner) ||
+            (b4.titleLabel?.text == winner && b5.titleLabel?.text == winner && b6.titleLabel?.text == winner) ||
+            (b7.titleLabel?.text == winner && b7.titleLabel?.text == winner && b9.titleLabel?.text == winner) ||
+            (b1.titleLabel?.text == winner && b4.titleLabel?.text == winner && b7.titleLabel?.text == winner) ||
+            (b2.titleLabel?.text == winner && b5.titleLabel?.text == winner && b8.titleLabel?.text == winner) ||
+            (b3.titleLabel?.text == winner && b6.titleLabel?.text == winner && b9.titleLabel?.text == winner) ||
+            (b1.titleLabel?.text == winner && b5.titleLabel?.text == winner && b9.titleLabel?.text == winner) ||
+            (b3.titleLabel?.text == winner && b5.titleLabel?.text == winner && b7.titleLabel?.text == winner)
+            
         {
-            if  (b1.titleLabel?.text == winner && b2.titleLabel?.text == winner && b3.titleLabel?.text == winner) ||
-                (b4.titleLabel?.text == winner && b5.titleLabel?.text == winner && b6.titleLabel?.text == winner) ||
-                (b7.titleLabel?.text == winner && b7.titleLabel?.text == winner && b9.titleLabel?.text == winner) ||
-                (b1.titleLabel?.text == winner && b4.titleLabel?.text == winner && b7.titleLabel?.text == winner) ||
-                (b2.titleLabel?.text == winner && b5.titleLabel?.text == winner && b8.titleLabel?.text == winner) ||
-                (b3.titleLabel?.text == winner && b6.titleLabel?.text == winner && b9.titleLabel?.text == winner) ||
-                (b1.titleLabel?.text == winner && b5.titleLabel?.text == winner && b9.titleLabel?.text == winner) ||
-                (b3.titleLabel?.text == winner && b5.titleLabel?.text == winner && b7.titleLabel?.text == winner)
-                
-            {
-                print("\(winner) is the winner")
-                let alertController = UIAlertController(title: "\(winner) Won", message: "", preferredStyle: .alert)
-                let restartAction = UIAlertAction(title: "Play Again", style: .cancel) { (alert) in
-                    self.restartGame()
-                }
-                alertController.addAction(restartAction)
-                present(alertController, animated: true, completion: nil)
-                
-                
+            print("\(winner) is the winner")
+     
+            scoreBoard(player: winner)
+            restartGame()
         }
+        
     }
+  
     
     func restartGame() {
         
@@ -91,10 +167,67 @@ class ViewController: UIViewController {
             b.setTitle("", for: .normal)
             b.titleLabel?.text = ""
             b.isEnabled = true
+            view.backgroundColor = randomColor()
+            
         }
         
         counter = 0
-        turnLabel.text = "X Turn"
+        turnLabel.text = "😼 Turn"
+      
     }
     
+    
+    func scoreBoard(player: String) {
+        if player == "😼" {
+            xcounter += 1
+            xScore.text = String(xcounter)
+        
+            self.restartGame()
+            
+        }
+        if player == "🐶" {
+            ocounter += 1
+            oScore.text = String(ocounter)
+           
+            self.restartGame()
+            
+        }
+        
+        if xcounter == 3 {
+            alert(winner: "😼")
+            xcounter = 0
+            ocounter = 0
+            oScore.text = ""
+            xScore.text = ""
+          self.restartGame()
+            playWhistle()
+        }
+        if ocounter == 3 {
+            alert(winner: "🐶")
+            ocounter = 0
+            xcounter = 0
+            oScore.text = ""
+            xScore.text = ""
+           self.restartGame()
+            playWhistle()
+        }
+        
+    }
+    
+   func randomColor() -> UIColor{
+       let red = CGFloat(drand48())
+       let green = CGFloat(drand48())
+       let blue = CGFloat(drand48())
+       return UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+   }
+    
+    func alert (winner : String)
+    {
+        let alertController = UIAlertController(title: "\(winner) Won", message: "", preferredStyle: .alert)
+        let restartAction = UIAlertAction(title: "Play Again", style: .cancel) { (alert) in
+            self.restartGame()
+        }
+        alertController.addAction(restartAction)
+        present(alertController, animated: true, completion: nil)
+    }
 }
